ed05b4f3293cb2424d4df5d1b702830866df9afa
Chara Marie Demetillo <demetillo.chara123@gmail.com>
2026-01-17 12:22:58 +0800

    Update Self_Exercise_#1.py

--- Self_Exercise_#1.py
+++ Self_Exercise_#1.py
@@ -10,18 +10,16 @@ user = input("From 0-100, enter the score you've got: ")
 score = int(user)
 
 if score >= 90 and score <= 100:
-    print(f"grade = A")
-    print(f"Excellent work")
-elif score >= 80 and score <= 89:
-    print(f"grade = B")
-    print(f"Satisfactory work")
-elif score >= 70 and score <= 79:
-    print(f"grade = C")
-    print(f"Fair job")
-elif score > 60 and score <= 69:
-    print(f"grade = D")
-    print(f"Barely failed, but good enough")
-else:
-    print(f"grade = F")
+    print("Graded A: Excellent work!")
 
-    print(f"PAGTUON LAGEH DILI MAG SALIG UG AI")
+elif score >= 80 and score <= 89:
+    print("Graded B: Excellent work!")
+
+elif score >= 70 and score <= 79:
+    print("Graded C: Fair Job!")
+
+elif score >= 60 and score <= 69:
+    print("Grade D: Barely")
+
+else:
+    print("Graded F")
